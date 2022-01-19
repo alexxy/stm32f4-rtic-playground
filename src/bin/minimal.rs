b50f7764a90d7212144c22c6a1abc465b3427d9f
Alexey Shvetsov <alexxyum@gmail.com>
2022-01-19 13:44:33 +0300

    Add simple files
    Signed-off-by: Alexey Shvetsov <alexxyum@gmail.com>

---
+++ src/bin/minimal.rs
@@ -0,0 +1,67 @@
+#![no_main]
+#![no_std]
+
+use f411_rtic_playground as _; // global logger + panicking-behavior + memory layout
+
+#[rtic::app(device = stm32f4xx_hal::pac, peripherals = true)]
+mod app {
+    use stm32f4xx_hal::{
+        gpio::{gpioa::PA0, gpioc::PC13, Edge, Input, Output, PullUp, PushPull},
+        prelude::*,
+    };
+
+    // Shared resources go here
+    #[shared]
+    struct Shared {}
+
+    // Local resources go here
+    #[local]
+    struct Local {
+        button: PA0<Input<PullUp>>,
+        led: PC13<Output<PushPull>>,
+    }
+
+    #[init]
+    fn init(mut ctx: init::Context) -> (Shared, Local, init::Monotonics) {
+        defmt::info!("init");
+        // syscfg
+        let mut syscfg = ctx.device.SYSCFG.constrain();
+        // gpio ports A and C
+        let gpioa = ctx.device.GPIOA.split();
+        let gpioc = ctx.device.GPIOC.split();
+        // button
+        let mut button = gpioa.pa0.into_pull_up_input();
+        button.make_interrupt_source(&mut syscfg);
+        button.enable_interrupt(&mut ctx.device.EXTI);
+        button.trigger_on_edge(&mut ctx.device.EXTI, Edge::Falling);
+        // led
+        let led = gpioc.pc13.into_push_pull_output();
+
+        (
+            Shared {
+               // Initialization of shared resources go here
+            },
+            Local {
+                // Initialization of local resources go here
+                button,
+                led
+            },
+            init::Monotonics(),
+        )
+    }
+
+    // Optional idle, can be removed if not needed.
+    #[idle]
+    fn idle(_: idle::Context) -> ! {
+        defmt::info!("idle");
+        loop {
+            continue;
+        }
+    }
+    
+    #[task(binds = EXTI0, local = [button, led])]
+    fn button_click(ctx: button_click::Context) {
+        ctx.local.button.clear_interrupt_pending_bit();
+        ctx.local.led.toggle();
+    }
+}
